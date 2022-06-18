39dd8c38883c0c0e4b0f1db8230edd599b5da3ef
R2turnTrue <46389044+R2turnTrue@users.noreply.github.com>
2022-06-18 20:47:10 +0900

    Handle exceptions & Load all scripts in script directory

--- src/main/kotlin/xyz/minejs/minepy/MinePyPlugin.kt
+++ src/main/kotlin/xyz/minejs/minepy/MinePyPlugin.kt
@@ -2,15 +2,16 @@ package xyz.minejs.minepy
 
 import org.bukkit.command.CommandExecutor
 import org.bukkit.plugin.java.JavaPlugin
+import org.python.core.PyException
 import org.python.util.PythonInterpreter
-import java.io.File
 import java.nio.file.Paths
 import java.util.*
+import kotlin.collections.ArrayList
 
 lateinit var plugin: MinePyPlugin
 
 class MinePyPlugin: JavaPlugin() {
-    lateinit var interpreter: PythonInterpreter
+    val interpreters = ArrayList<PythonInterpreter>()
 
     override fun onEnable() {
         plugin = this
@@ -22,25 +23,39 @@ class MinePyPlugin: JavaPlugin() {
         PythonInterpreter.initialize(System.getProperties(), props, arrayOf(""))
         loadScript()
         getCommand("pyreload")?.setExecutor(CommandExecutor { sender, command, label, args ->
-            val endFunction = interpreter.get("end")
-            if(endFunction != null && endFunction.isCallable)
-                endFunction.__call__()
-            interpreter.close()
-            loadScript()
-            sender.sendMessage("Re-loaded script(s)!")
+            for (interpreter in interpreters) {
+                val endFunction = interpreter.get("end")
+                if(endFunction != null && endFunction.isCallable)
+                    endFunction.__call__()
+                interpreter.close()
+            }
+            val errors = loadScript()
+            errors.forEach { error ->
+                sender.sendMessage("Error when loading ${error.key} - ${error.value.message}")
+            }
+            sender.sendMessage("Re-loaded script(s) with ${errors.size} error(s)!")
             return@CommandExecutor true
         })
     }
 
-    fun loadScript() {
-        val scriptFile = File(dataFolder, "main.py")
+    fun loadScript(): WeakHashMap<String, PyException> {
         if(!dataFolder.exists())
             dataFolder.mkdir()
-        if(!scriptFile.exists())
-            scriptFile.createNewFile()
 
-        interpreter = PythonInterpreter()
-        interpreter.systemState.classLoader = this.classLoader
-        interpreter.execfile(scriptFile.inputStream())
+        val exceptionMap = WeakHashMap<String, PyException>()
+
+        dataFolder.listFiles()?.forEach { scriptFile ->
+            try {
+                interpreters.add(PythonInterpreter().also { interpreter ->
+                    interpreter.systemState.classLoader = this.classLoader
+                    interpreter.execfile(scriptFile.inputStream())
+                })
+            } catch (ex: PyException) {
+                ex.printStackTrace()
+                exceptionMap[scriptFile.name] = ex
+            }
+        }
+
+        return exceptionMap
     }
 }
